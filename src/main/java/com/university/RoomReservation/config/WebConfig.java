6f0bdbe9b7dfcd89e66f7143b45a9e29faed25d6
DaniloCerovic87 <chessteacher87@gmail.com>
2024-12-20 19:31:54 +0100

    Moving the logic for message source to application.yml

--- src/main/java/com/university/RoomReservation/config/WebConfig.java
+++ src/main/java/com/university/RoomReservation/config/WebConfig.java
@@ -1,10 +1,7 @@
 package com.university.RoomReservation.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,14 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(headerLocaleChangeInterceptor);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:i18n/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
     }
 
 }
