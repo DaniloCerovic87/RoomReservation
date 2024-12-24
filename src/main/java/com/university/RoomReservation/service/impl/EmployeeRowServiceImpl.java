4dfc875840945fc21f7649ba65eb0386ab81b6e4
DaniloCerovic87 <chessteacher87@gmail.com>
2024-12-24 15:29:46 +0100

    Logic for ignoring empty rows and validation for headers added

--- src/main/java/com/university/RoomReservation/service/impl/EmployeeRowServiceImpl.java
+++ src/main/java/com/university/RoomReservation/service/impl/EmployeeRowServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -35,10 +36,18 @@ public class EmployeeRowServiceImpl implements EmployeeRowService {
         List<EmployeeRowDTO> importedEmployeeData = new ArrayList<>();
         try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
             Sheet sheet = workbook.getSheetAt(0);
+            if (!areHeadersValid(sheet)) {
+                return Collections.emptyList();
+            }
 
             DataFormatter dataFormatter = new DataFormatter();
             for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                 Row row = sheet.getRow(rowIndex);
+                if (isRowEmpty(row)) {
+                    log.debug("Row at index {} is empty ", rowIndex);
+                    continue;
+                }
+
                 EmployeeRowDTO employeeRowDTO = EmployeeRowDTO.builder()
                         .firstName(getCellValue(row, 0, dataFormatter))
                         .lastName(getCellValue(row, 1, dataFormatter))
@@ -59,6 +68,40 @@ public class EmployeeRowServiceImpl implements EmployeeRowService {
             log.error("An error occurred while trying to parse the excel file: {}", file.getOriginalFilename(), e);
             return Collections.emptyList();
         }
+    }
+
+    private boolean isRowEmpty(Row row) {
+        if (row == null) {
+            return true;
+        }
+
+        for (int i = 0; i < row.getPhysicalNumberOfCells(); i++) {
+            String cellValue = getCellValue(row, i, new DataFormatter());
+
+            if (cellValue != null && !cellValue.trim().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean areHeadersValid(Sheet sheet) {
+        Row firstRow = sheet.getRow(0);
+        if (firstRow == null) {
+            log.error("The first row (header) is missing.");
+            return false;
+        }
+
+        List<String> expectedHeaders = Arrays.asList("firstName", "lastName", "personalId", "email", "title", "department");
+        for (int i = 0; i < expectedHeaders.size(); i++) {
+            String header = getCellValue(firstRow, i, new DataFormatter());
+            if (!expectedHeaders.get(i).equalsIgnoreCase(header)) {
+                log.error("Invalid header at column {}. Expected: {} but found: {}", i, expectedHeaders.get(i), header);
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
