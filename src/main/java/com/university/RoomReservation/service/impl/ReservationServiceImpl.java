732009aff8b37a2ff31b410b989da159028c586e
DaniloCerovic87 <chessteacher87@gmail.com>
2024-12-28 14:47:16 +0100

    Unused import removed

--- src/main/java/com/university/RoomReservation/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/university/RoomReservation/service/impl/ReservationServiceImpl.java
@@ -20,7 +20,6 @@ import com.university.RoomReservation.service.UserService;
 import com.university.RoomReservation.util.ReservationValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
